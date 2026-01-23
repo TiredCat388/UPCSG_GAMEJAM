3850800d132c52147c634f3e7bdcd8516284bc84
andee <andeellenes@gmail.com>
2026-01-23 12:38:48 +0800

    [PlayerMovement][Llenes] Merged PlayerDash into PlayerMovement

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -1,19 +1,46 @@
 extends CharacterBody2D
 
-var player_body: CharacterBody2D
-var player_dash: PlayerDash
-
 @export var MAX_SPEED: float = 400.0
 @export var ACCELERATION: float = 1600.0
 @export var DECELERATION: float = 2000.0
 
 var facing_direction: Vector2 = Vector2.RIGHT
 
+#region PlayerDash
+@export var dash_speed: float = 800.0
+@export var dash_duration: float = 0.2
+@export var dash_cooldown: float = 0.5
 
-func _ready() -> void:
-	player_dash = $PlayerDash
-	assert(player_dash != null, "PlayerDash node not found")
+var is_dashing: bool = false
+var can_dash: bool = true
+var dash_direction: Vector2 = Vector2.ZERO
 
+
+func _start_dash() -> void:
+	velocity = dash_direction.normalized() * dash_speed
+	await get_tree().create_timer(dash_duration).timeout
+	is_dashing = false
+
+	# Starts dash cooldown timer
+	await get_tree().create_timer(dash_cooldown).timeout
+	
+	can_dash = true
+
+
+func try_dash(input_direction: Vector2) -> void:
+	# Prevents dashing while on cooldown or while currently dashing
+	if not can_dash or is_dashing:
+		return
+
+	dash_direction = input_direction
+	if dash_direction == Vector2.ZERO:
+		dash_direction = facing_direction
+
+	is_dashing = true
+	can_dash = false
+
+	_start_dash()
+#endregion
 
 func _physics_process(delta: float) -> void:
 	# Normalizes player input to handle diagonal movement properly
@@ -22,10 +49,10 @@ func _physics_process(delta: float) -> void:
 		facing_direction = input_direction.normalized()
 
 	if Input.is_action_just_pressed("dash"):
-		player_dash.try_dash(input_direction)
+		try_dash(input_direction)
 
-	if player_dash.is_dashing:
-		velocity = player_dash.dash_direction * player_dash.dash_speed
+	if is_dashing:
+		velocity = dash_direction * dash_speed
 	else:
 		# Separates target from current velocity to allow acceleration and deceleration
 		var target_velocity: Vector2 = input_direction * MAX_SPEED
